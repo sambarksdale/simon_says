dbe01877306f1ec45f6c52efe71c81a05c369cae
Samuel Barksdale <samuelbarksdale@Samuels-MacBook-Air.local>
2019-04-09 15:24:27 -0400

    added delay to loop iterations

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const staticArray = [0,1,2,3]
 
 let comp = {
-    compArray: [],
+    compArray: [0,3,1,3,2,3,3,2,1,0,3,1,3,2,3,3,2,1,0,3,1,3,2,3,3,2,1,0,3,1,3,2,3,3,2,1,0,3,1,3,2,3,3,2,1,0,3,1,3,2,3,3,2,1],
     isTurn: true
 }
 
@@ -21,24 +21,29 @@ function getRandomValue(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }    
 
-
 function addCompArrayValue(){
     comp.compArray.push(getRandomValue(staticArray.length));
 }
-
-
+//determines length of lightup/sound
+let duration = 500;
 
 function lightUpButton(id){
-    let startColor = $(id).css('background');
-    $(id).css('background','black')
+    let startColor = $('#B'+id).css('background');
+    $('#B'+id).css('background','black')
     setTimeout(function(){
-        $(id).css('background',startColor)
-    },1000) 
+        $('#B'+id).css('background',startColor)
+    },500) 
 }
 
 $(function(){
-    lightUpButton('#B4');
+    for(let i = 0; i < comp.compArray.length; i++){
+        setTimeout(function(){
+            lightUpButton(comp.compArray[i]);  
+        },i * 600)
+        
+    }
 })
+
 
    
 
