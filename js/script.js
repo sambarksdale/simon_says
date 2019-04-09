a06e25020d3f0fe4c88e218b6540e3e0a629f127
Samuel Barksdale <samuelbarksdale@Samuels-MacBook-Air.local>
2019-04-09 14:30:29 -0400

    added flashing functionality

--- js/script.js
+++ js/script.js
@@ -15,4 +15,32 @@ let player = {
         }
     }
 }
+//gets random number
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomValue(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}    
+
+
+function addCompArrayValue(){
+    comp.compArray.push(getRandomValue(staticArray.length));
+}
+
+
+
+function lightUpButton(id){
+    let startColor = $(id).css('background');
+    $(id).css('background','black')
+    setTimeout(function(){
+        $(id).css('background',startColor)
+    },1000) 
+}
+
+$(function(){
+    lightUpButton('#B4');
+})
+
+   
+
+
 
